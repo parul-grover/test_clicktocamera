1fdcc067ceb8cefcc249708250ad8ced0e70e219
Ankur Mittal <ankurmittal092@gmail.com>
2017-04-21 00:22:43 +0530

    Displaying captured image

--- src/js/build.js
+++ src/js/build.js
@@ -4,4 +4,20 @@ console.log('about to fire');
 
 $(document).ready(function() {
 	
+	function readURL(input) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+
+	        reader.onload = function (e) {
+	            $('#imgView').attr('src', e.target.result).show();
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
+
+	$("#uploadImage").change(function(){
+	    readURL(this);
+	});
+
 });
