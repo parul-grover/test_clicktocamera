b0f1038bac81907eb3100864a3eb2dafa25b94f0
Ankur Mittal <ankurmittal092@gmail.com>
2017-04-22 15:54:24 +0530

    Show hide complete + Facebook init done

--- src/js/build.js
+++ src/js/build.js
@@ -1,23 +1,55 @@
 
-
-console.log('about to fire');
-
 $(document).ready(function() {
-	
+	var actionsWrpDOM = $(".actionsWrpJS"),
+	imgViewDOM = $('#imgView'),
+	uploadImageBtnDOM = $("#uploadImageBtn"),
+	uploadBtnContDOM = $(".uploadBtnContJS"),
+	acceptBtnDOM = $(".acceptBtnJS"),
+	deleteBtnDOM = $(".delBtnJS"),
+	filesObj = {};
+
 	function readURL(input) {
 	    if (input.files && input.files[0]) {
 	        var reader = new FileReader();
+	        
+	        filesObj = input.files[0];
 
 	        reader.onload = function (e) {
-	            $('#imgView').attr('src', e.target.result).show();
+	            imgViewDOM.attr('src', e.target.result).show();
+	            actionsWrpDOM.show();
+	            uploadBtnContDOM.hide();
 	        }
 
 	        reader.readAsDataURL(input.files[0]);
 	    }
 	}
 
-	$("#uploadImage").change(function(){
+	uploadImageBtnDOM.change(function(){
 	    readURL(this);
+	});
+
+	acceptBtnDOM.click(function(){
+		console.log('++++ Calling accept button', filesObj);
+		FB.getLoginStatus(function (res) { 
+		  if(res.authResponse){
+		    console.log('User authenticated');
+		  }
+
+		  else {
+		    //request for permissions
+		    console.log('Kindly request for permissions');
+		  }
+		});
+	});
+
+	deleteBtnDOM.click(function(){
+		console.log('---- Clicking reject button');
+		uploadBtnContDOM.show();
+		actionsWrpDOM.hide();
+
+		imgViewDOM.hide();
+		filesObj = {};
+
 	});
 
 });
